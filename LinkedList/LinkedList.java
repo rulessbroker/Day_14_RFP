7f1f56d529f12485c7f72e09bbf2944fb4b62461
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-28 12:32:10 +0530

    append LinkedList

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -40,9 +40,13 @@ public class LinkedList<K> {
 	public static void main(String[] args) {
 		LinkedList linkedList = new LinkedList();
 
-		linkedList.add(70);
-		linkedList.add(30);
-		linkedList.add(56);
+//		linkedList.add(70);
+//		linkedList.add(30);
+//		linkedList.add(56);
+		
+		linkedList.append(56);
+		linkedList.append(30);
+		linkedList.append(70);
 
 		linkedList.print();
 	}
